e1387f96b224cc62c44e1765c8c408777f9f4201
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2019-10-17 20:46:42 -0300

    adicionado react-router-dom e criado as primeiras rotas da pagina

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => <h2>HATS PAGE</h2>;
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div style={{ backgroundImage: `url(${imageUrl})` }} className={`${size} menu-item`}>
-    <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
+  return (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="background-image"
+      ></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
-export default MenuItem;
+  );
+};
+export default withRouter(MenuItem);
