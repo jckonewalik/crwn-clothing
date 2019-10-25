f7b56e70d528e97dcb97e4d00b14de352eb1b4a2
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2019-10-25 07:31:19 -0300

    criado o component de carrinho e dropdown do carrinho

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './cart-icon.styles.scss';
+
+import { ReactComponent as ShoppingBagIcon } from '../../assets/shopping-bag.svg';
+
+const CartIcon = () => (
+  <div className="cart-icon">
+    <ShoppingBagIcon className="shopping-icon" />
+    <span className="item-count">0</span>
+  </div>
+);
+
+export default CartIcon;
