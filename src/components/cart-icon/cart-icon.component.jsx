95a1263e4e87b0db3e2ab5777c4b194afa3cc3b7
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2019-10-29 07:16:00 -0300

    criado o selector para guardar o cache do usuario

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,18 +3,19 @@ import './cart-icon.styles.scss';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsAmount } from '../../redux/cart/cart.selectors';
+import { createStructuredSelector } from 'reselect';
 
 import { ReactComponent as ShoppingBagIcon } from '../../assets/shopping-bag.svg';
 
 const CartIcon = ({ toggleCartHidden, itemAmount }) => (
-  <div className="cart-icon">
-    <ShoppingBagIcon className="shopping-icon" onClick={() => toggleCartHidden()} />
+  <div className="cart-icon" onClick={() => toggleCartHidden()}>
+    <ShoppingBagIcon className="shopping-icon" />
     <span className="item-count">{itemAmount}</span>
   </div>
 );
 
-const mapStateToProps = state => ({
-  itemAmount: selectCartItemsAmount(state),
+const mapStateToProps = createStructuredSelector({
+  itemAmount: selectCartItemsAmount,
 });
 
 const mapDispatchToProps = dispatch => {
