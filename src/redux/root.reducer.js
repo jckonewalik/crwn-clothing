1efacf9717b1bc9be70fc39731d12efdeec24171
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2019-10-29 21:01:15 -0300

    adicionado biblioteca redux-persist para guardar os itens do carrinho no localStorage

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -2,9 +2,21 @@ import { combineReducers } from 'redux';
 import { userReducer } from './user/user.reducer';
 import { cartReducer } from './cart/cart.reducer';
 
+import { persistReducer } from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
 
-export default rootReducer;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default persistedReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
-import rootReducer from './root.reducer';
+import persistedReducer from './root.reducer';
 import { logger } from 'redux-logger';
+
+import { persistStore } from 'redux-persist';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(persistedReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
