09184469a61868ea4ff0427fc729202ef7334ae2
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2019-11-05 07:20:30 -0300

    adicionado evento de click no titulo na CollectionPreview para direcionar a pagina de Collection

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-
-import CollectionItem from '../collection-item/collection-item.component'
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import CollectionItem from '../collection-item/collection-item.component';
 import {
   CollectionPreviewContainer,
   TitleStyled,
   PreviewContainer,
-} from './collection-preview.styles'
+} from './collection-preview.styles';
 
-const CollectionPreview = ({ title, items }) => (
-  <CollectionPreviewContainer>
-    <TitleStyled>{title.toUpperCase()}</TitleStyled>
-    <PreviewContainer>
-      {items
-        .filter((item, idx) => idx < 4)
-        .map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-    </PreviewContainer>
-  </CollectionPreviewContainer>
-)
+const CollectionPreview = ({ title, items, routeName, match, history }) => {
+  return (
+    <CollectionPreviewContainer>
+      <TitleStyled onClick={() => history.push(`${match.path}/${routeName}`)}>
+        {title.toUpperCase()}
+      </TitleStyled>
+      <PreviewContainer>
+        {items
+          .filter((item, idx) => idx < 4)
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </PreviewContainer>
+    </CollectionPreviewContainer>
+  );
+};
 
-export default CollectionPreview
+export default withRouter(CollectionPreview);
--- src/components/collection-preview/collection-preview.styles.js
+++ src/components/collection-preview/collection-preview.styles.js
@@ -1,17 +1,21 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const CollectionPreviewContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 30px;
-`
+`;
 
 export const TitleStyled = styled.h1`
   font-size: 28px;
   margin-bottom: 25px;
-`
+
+  :hover {
+    cursor: pointer;
+  }
+`;
 
 export const PreviewContainer = styled.div`
   display: flex;
   justify-content: space-between;
-`
+`;
